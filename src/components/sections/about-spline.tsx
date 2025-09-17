88da202c840d6145dc320ed16d8b118fa4b2b3c9
ABBEY AINA <olamilekansunday445@gmail.com>
2025-09-17 14:53:21 +0000

    put this in the about section, where it would look creative. this is the

---
+++ src/components/sections/about-spline.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const Spline = dynamic(() => import('@splinetool/react-spline'), {
+  ssr: false,
+});
+
+export default function AboutSpline() {
+  return (
+    <section id="about-spline" className="py-20 sm:py-32">
+      <div className="container mx-auto px-4 h-[400px] md:h-[500px]">
+        <Suspense fallback={<div className="w-full h-full bg-card/50 rounded-lg animate-pulse" />}>
+          <Spline scene="https://prod.spline.design/PFrf9m-mSJfKX8Ul/scene.splinecode" />
+        </Suspense>
+      </div>
+    </section>
+  );
+}
