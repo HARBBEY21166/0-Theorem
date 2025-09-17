eafa4c06b73d0e46333692dea8b3fcb1c4091791
ABBEY AINA <olamilekansunday445@gmail.com>
2025-09-17 14:58:05 +0000

    added spline design

--- src/components/sections/about-spline.tsx
+++ src/components/sections/about-spline.tsx
@@ -12,7 +12,7 @@ export default function AboutSpline() {
     <section id="about-spline" className="py-20 sm:py-32">
       <div className="container mx-auto px-4 h-[400px] md:h-[500px]">
         <Suspense fallback={<div className="w-full h-full bg-card/50 rounded-lg animate-pulse" />}>
-          <Spline scene="https://prod.spline.design/PFrf9m-mSJfKX8Ul/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/TWERz0weQDv7pm3S/scene.splinecode" />
         </Suspense>
       </div>
     </section>
