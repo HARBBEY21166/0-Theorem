57ebe6c04c7655aa2b8e4c0edce98a2df140cd3f
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2025-10-01 10:42:07 +0000

    its working now. also the loading animation should displaying when a rel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -7,22 +9,41 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import { ThemeProvider } from '@/components/layout/theme-provider';
 import { inter, poppins } from '@/app/fonts';
-
-export const metadata: Metadata = {
-  title: 'Zero Theorem',
-  description: 'Where precise software engineering meets visionary graphic design to build foundational solutions.',
-};
+import { useState, useEffect } from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time for demonstration, remove in production
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // Adjust time as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning className={`${inter.variable} ${poppins.variable}`}>
       <head>
       </head>
       <body className={cn("font-body antialiased")}>
+        {isLoading && (
+            <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-background z-50">
+                <div className="w-64 h-64">
+                    <DotLottieReact
+                    src="https://lottie.host/f0e7431f-49f5-428c-8d8a-a625216ebbe7/W3UqdgLOaR.lottie"
+                    loop
+                    autoplay
+                    />
+                </div>
+            </div>
+        )}
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
