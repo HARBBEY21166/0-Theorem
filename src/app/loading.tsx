778863cc8a4072a1079692785c03e651856be440
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2025-10-01 10:45:03 +0000

    when i click on a menu page. the loader doesnt show anymore. it only sho

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+
+"use client";
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-background z-50">
+        <div className="w-64 h-64">
+            <DotLottieReact
+            src="https://lottie.host/f0e7431f-49f5-428c-8d8a-a625216ebbe7/W3UqdgLOaR.lottie"
+            loop
+            autoplay
+            />
+        </div>
+    </div>
+  );
+}
